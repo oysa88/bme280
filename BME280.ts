7aece5d0135dd306135172dec52462f2412b8239
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2026-02-06 10:39:47 +0100

    Oppdater BME280.ts

--- BME280.ts
+++ BME280.ts
@@ -19,88 +19,120 @@ enum BME280_P {
     hPa = 1
 }
 
-/**
- * BME280 block
- */
 //% weight=40 color=#70c0f0 icon="\uf2c9" block="BME280"
 //% groups=["Målinger", "Hendelser", "Oppsett"]
 namespace BME280 {
-    let BME280_I2C_ADDR = BME280_I2C_ADDRESS.ADDR_0x77
 
-    function setreg(reg: number, dat: number): void {
-        let buf = pins.createBuffer(2);
-        buf[0] = reg;
-        buf[1] = dat;
-        pins.i2cWriteBuffer(BME280_I2C_ADDR, buf);
-    }
+    let BME280_I2C_ADDR = BME280_I2C_ADDRESS.ADDR_0x76
+    let initialized = false
 
-    function getreg(reg: number): number {
-        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt8BE);
-    }
+    let dig_T1 = 0
+    let dig_T2 = 0
+    let dig_T3 = 0
+    let dig_P1 = 0
+    let dig_P2 = 0
+    let dig_P3 = 0
+    let dig_P4 = 0
+    let dig_P5 = 0
+    let dig_P6 = 0
+    let dig_P7 = 0
+    let dig_P8 = 0
+    let dig_P9 = 0
+    let dig_H1 = 0
+    let dig_H2 = 0
+    let dig_H3 = 0
+    let dig_H4 = 0
+    let dig_H5 = 0
+    let dig_H6 = 0
 
-    function getInt8LE(reg: number): number {
-        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int8LE);
-    }
-
-    function getUInt16LE(reg: number): number {
-        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt16LE);
-    }
-
-    function getInt16LE(reg: number): number {
-        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int16LE);
-    }
-
-    let dig_T1 = getUInt16LE(0x88)
-    let dig_T2 = getInt16LE(0x8A)
-    let dig_T3 = getInt16LE(0x8C)
-    let dig_P1 = getUInt16LE(0x8E)
-    let dig_P2 = getInt16LE(0x90)
-    let dig_P3 = getInt16LE(0x92)
-    let dig_P4 = getInt16LE(0x94)
-    let dig_P5 = getInt16LE(0x96)
-    let dig_P6 = getInt16LE(0x98)
-    let dig_P7 = getInt16LE(0x9A)
-    let dig_P8 = getInt16LE(0x9C)
-    let dig_P9 = getInt16LE(0x9E)
-    let dig_H1 = getreg(0xA1)
-    let dig_H2 = getInt16LE(0xE1)
-    let dig_H3 = getreg(0xE3)
-    let a = getreg(0xE5)
-    let dig_H4 = (getreg(0xE4) << 4) + (a % 16)
-    let dig_H5 = (getreg(0xE6) << 4) + (a >> 4)
-    let dig_H6 = getInt8LE(0xE7)
-    setreg(0xF2, 0x04)
-    setreg(0xF4, 0x2F)
-    setreg(0xF5, 0x0C)
     let T = 0
     let P = 0
     let H = 0
 
+    function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = dat
+        pins.i2cWriteBuffer(BME280_I2C_ADDR, buf)
+    }
+
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE)
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt8BE)
+    }
+
+    function getInt8LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE)
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int8LE)
+    }
+
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE)
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt16LE)
+    }
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE)
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int16LE)
+    }
+
+    function init() {
+        if (initialized) return
+
+        dig_T1 = getUInt16LE(0x88)
+        dig_T2 = getInt16LE(0x8A)
+        dig_T3 = getInt16LE(0x8C)
+
+        dig_P1 = getUInt16LE(0x8E)
+        dig_P2 = getInt16LE(0x90)
+        dig_P3 = getInt16LE(0x92)
+        dig_P4 = getInt16LE(0x94)
+        dig_P5 = getInt16LE(0x96)
+        dig_P6 = getInt16LE(0x98)
+        dig_P7 = getInt16LE(0x9A)
+        dig_P8 = getInt16LE(0x9C)
+        dig_P9 = getInt16LE(0x9E)
+
+        dig_H1 = getreg(0xA1)
+        dig_H2 = getInt16LE(0xE1)
+        dig_H3 = getreg(0xE3)
+        let a = getreg(0xE5)
+        dig_H4 = (getreg(0xE4) << 4) + (a & 0x0F)
+        dig_H5 = (getreg(0xE6) << 4) + (a >> 4)
+        dig_H6 = getInt8LE(0xE7)
+
+        setreg(0xF2, 0x04)
+        setreg(0xF4, 0x2F)
+        setreg(0xF5, 0x0C)
+
+        initialized = true
+    }
+
     function get(): void {
+        if (!initialized) init()
+
         let adc_T = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
         let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
         let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
         let t = var1 + var2
-        let T_x100 = (t * 5 + 128) >> 8   // 0.01 °C
-        T = Math.idiv(T_x100, 10)         // 0.1 °C lagres i T
+        let T_x100 = (t * 5 + 128) >> 8
+        T = Math.idiv(T_x100, 10)
+
         var1 = (t >> 1) - 64000
         var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
         var2 = var2 + ((var1 * dig_P5) << 1)
         var2 = (var2 >> 2) + (dig_P4 << 16)
-        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
+        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + ((dig_P2 * var1) >> 1)) >> 18
         var1 = ((32768 + var1) * dig_P1) >> 15
-        if (var1 == 0)
-            return; // avoid exception caused by division by zero
+        if (var1 == 0) return
+
         let adc_P = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
         let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
-        _p = Math.idiv(_p, var1) * 2;
+        _p = Math.idiv(_p, var1) * 2
         var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
         var2 = (((_p >> 2)) * dig_P8) >> 13
         P = _p + ((var1 + var2 + dig_P7) >> 4)
+
         let adc_H = (getreg(0xFD) << 8) + getreg(0xFE)
         var1 = t - 76800
         var2 = (((adc_H << 14) - (dig_H4 << 20) - (dig_H5 * var1)) + 16384) >> 15
@@ -111,182 +143,43 @@ namespace BME280 {
         H = (var2 >> 12) >> 10
     }
 
-    /**
-     * get pressure
-     */
-    //% blockId="BME280_GET_PRESSURE" block="Trykk %u"
+    //% block="Trykk %u"
     //% group="Målinger"
-    //% weight=79 blockGap=8
     export function pressure(u: BME280_P): number {
-        get();
-        if (u == BME280_P.Pa) return P;
-        else return Math.idiv(P, 100)
+        get()
+        return u == BME280_P.Pa ? P : Math.idiv(P, 100)
     }
 
-    /**
-     * get temperature
-     */
-    //% blockId="BME280_GET_TEMPERATURE" block="Temperatur %u"
+    //% block="Temperatur %u"
     //% group="Målinger"
-    //% weight=80 blockGap=8
     export function temperature(u: BME280_T): number {
-        get();
-        if (u == BME280_T.T_C) return T / 10;
-        else return 32 + (T / 10) * 9 / 5;
+        get()
+        return u == BME280_T.T_C ? T / 10 : 32 + (T / 10) * 9 / 5
     }
 
-    /**
-     * get humidity
-     */
-    //% blockId="BME280_GET_HUMIDITY" block="Luftfuktighet"
+    //% block="Luftfuktighet"
     //% group="Målinger"
-    //% weight=78 blockGap=8
     export function humidity(): number {
-        get();
-        return H;
+        get()
+        return H
     }
 
-    /**
-     * power on
-     */
-    //% blockId="BME280_POWER_ON" block="Skru PÅ"
+    //% block="Skru PÅ"
     //% group="Oppsett"
-    //% weight=22 blockGap=8
     export function PowerOn() {
-        setreg(0xF4, 0x2F)
+        init()
     }
 
-    /**
-     * power off
-     */
-    //% blockId="BME280_POWER_OFF" block="Skru AV"
+    //% block="Skru AV"
     //% group="Oppsett"
-    //% weight=21 blockGap=8
     export function PowerOff() {
         setreg(0xF4, 0)
     }
 
-    /**
-     * Calculate Dewpoint
-     */
-    //% block="Duggpunkt"
-    //% group="Målinger"
-    //% weight=76 blockGap=8
-    export function Dewpoint(): number {
-        get();
-        let tempC = T / 10          // gjør om fra tidels grader til °C
-        return tempC - (100 - H) / 5
-    }
-
-    /**
-     * Pressure below Event
-     */
-    //% block="Trykk lavere enn %dat" dat.defl=100000
-    //% group="Hendelser"
-    export function PressureBelowThan(dat: number, body: () => void): void {
-        control.inBackground(function () {
-            while (true) {
-                get()
-                if (P < dat) {
-                    body()
-                }
-                basic.pause(1000)
-            }
-        })
-    }
-
-    /**
-     * Pressure higher Event
-     */
-    //% block="Trykk høyere enn %dat" dat.defl=100000
-    //% group="Hendelser"
-    export function PressureHigherThan(dat: number, body: () => void): void {
-        control.inBackground(function () {
-            while (true) {
-                get()
-                if (P > dat) {
-                    body()
-                }
-                basic.pause(1000)
-            }
-        })
-    }
-
-    /**
-     * humidity below Event
-     */
-    //% block="Luftfuktighet lavere enn %dat" dat.defl=10
-    //% group="Hendelser"
-    export function HumidityBelowThan(dat: number, body: () => void): void {
-        control.inBackground(function () {
-            while (true) {
-                get()
-                if (H < dat) {
-                    body()
-                }
-                basic.pause(1000)
-            }
-        })
-    }
-
-    /**
-     * humidity higher Event
-     */
-    //% block="Luftfuktighet høyere enn %dat" dat.defl=50
-    //% group="Hendelser"
-    export function HumidityHigherThan(dat: number, body: () => void): void {
-        control.inBackground(function () {
-            while (true) {
-                get()
-                if (H > dat) {
-                    body()
-                }
-                basic.pause(1000)
-            }
-        })
-    }
-
-    /**
-     * temperature below Event
-     */
-    //% block="Temperatur lavere enn %dat" dat.defl=10
-    //% group="Hendelser"
-    export function TemperatureBelowThan(dat: number, body: () => void): void {
-        control.inBackground(function () {
-            while (true) {
-                get()
-                if (T < dat) {
-                    body()
-                }
-                basic.pause(1000)
-            }
-        })
-    }
-
-    /**
-     * temperature higher Event
-     */
-    //% block="Temperatur høyere enn %dat" dat.defl=30
-    //% group="Hendelser"
-    export function TemperatureHigherThan(dat: number, body: () => void): void {
-        control.inBackground(function () {
-            while (true) {
-                get()
-                if (T > dat) {
-                    body()
-                }
-                basic.pause(1000)
-            }
-        })
-    }
-
-    /**
-     * set I2C address
-     */
-    //% blockId="BME280_SET_ADDRESS" block="Sett adresse %addr"
+    //% block="Sett adresse %addr"
     //% group="Oppsett"
-    //% weight=20 blockGap=8
     export function Address(addr: BME280_I2C_ADDRESS) {
         BME280_I2C_ADDR = addr
+        initialized = false
     }
 }
